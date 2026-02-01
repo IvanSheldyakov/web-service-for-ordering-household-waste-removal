ac28918ed7f381ad42c0af68ae0ab04c4becdeeb
Иван Павлович Шелдяков <i.sheldyakov@g.nsu.ru>
2026-02-01 17:57:25 +0700

    first commit

---
+++ src/main/java/ru/nsu/waste/removal/ordering/service/WasteRemovalOrderingService.java
@@ -0,0 +1,13 @@
+package ru.nsu.waste.removal.ordering.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteRemovalOrderingService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteRemovalOrderingService.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nsu/waste/removal/ordering/service/WasteRemovalOrderingServiceTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.waste.removal.ordering.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteRemovalOrderingServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
