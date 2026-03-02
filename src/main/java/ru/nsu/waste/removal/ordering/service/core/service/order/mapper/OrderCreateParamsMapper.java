b9747aa7ed5bf257232c24e7110de62945c59bd8
Иван Павлович Шелдяков <i.sheldyakov@g.nsu.ru>
2026-03-02 12:20:53 +0700

    добавлено создание заказа

---
+++ src/main/java/ru/nsu/waste/removal/ordering/service/core/service/order/mapper/OrderCreateParamsMapper.java
@@ -0,0 +1,24 @@
+package ru.nsu.waste.removal.ordering.service.core.service.order.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ru.nsu.waste.removal.ordering.service.core.model.order.OrderCreateParams;
+import ru.nsu.waste.removal.ordering.service.core.model.order.SlotOption;
+import ru.nsu.waste.removal.ordering.service.core.model.user.UserGreenSlotContext;
+
+@Mapper(componentModel = "spring")
+public interface OrderCreateParamsMapper {
+
+    @Mapping(target = "pickupFrom", source = "selectedSlot.pickupFrom")
+    @Mapping(target = "pickupTo", source = "selectedSlot.pickupTo")
+    @Mapping(target = "greenChosen", source = "selectedSlot.green")
+    @Mapping(target = "postalCode", source = "userContext.postalCode")
+    @Mapping(target = "costPoints", source = "costPoints")
+    OrderCreateParams toOrderCreateParams(
+            long userId,
+            String type,
+            SlotOption selectedSlot,
+            UserGreenSlotContext userContext,
+            long costPoints
+    );
+}
